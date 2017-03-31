f1b0ace139b19479ab453cc51c40e16e07145611
Kolovaitis <kna.209@yandex.ru>
2017-03-31 20:22:34 +0300

    Error colored buttons removing

--- app/src/main/java/by/superteam/kids/DrawActivity.java
+++ app/src/main/java/by/superteam/kids/DrawActivity.java
@@ -16,19 +16,21 @@ import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class DrawActivity extends AppCompatActivity implements View.OnTouchListener {
-    int currentColor=Color.DKGRAY;
+    int currentColor = Color.DKGRAY;
     int a;
-    Object[][]colors={
-            {"white",Color.WHITE},
-            {"red",Color.RED},
-            {"green",Color.GREEN},
-            {"blue",Color.BLUE},
-            {"yellow",Color.YELLOW},
-            {"cyan",Color.CYAN},
-            {"pink",Color.MAGENTA},
-            {"brown",Color.rgb(101,67,33)},
-            {"grey",Color.DKGRAY}
+    Object[][] colors = {
+            {"white", Color.WHITE},
+            {"red", Color.RED},
+            {"green", Color.GREEN},
+            {"blue", Color.BLUE},
+            {"yellow", Color.YELLOW},
+            {"cyan", Color.CYAN},
+            {"pink", Color.MAGENTA},
+            {"brown", Color.rgb(101, 67, 33)},
+            {"grey", Color.DKGRAY}
     };
     public static Object[][][][] levels = {
             {
@@ -49,10 +51,43 @@ public class DrawActivity extends AppCompatActivity implements View.OnTouchListe
                     }
             }
     };
+    ArrayList<ArrayList<Object>> errors = new ArrayList<>();
+    Thread errorThread = new Thread(new Runnable() {
+        @Override
+        public void run() {
+            while (DrawActivity.this.isFinishing() == false) {
+                for (int i = 0; i < errors.size(); i++) {
+                    final RectView view = (RectView) ((LinearLayout) DrawActivity.this.humanSide.getChildAt((int) errors.get(i).get(0))).getChildAt((int) errors.get(i).get(1));
+                    float newAlpha = (1f - (float) errors.get(i).get(2) - 0.1f);
+                    view.setAlpha(newAlpha);
 
+                    errors.get(i).set(2, newAlpha);
+                    if (newAlpha < 0.2) {
+                        view.setAlpha(1);
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                view.changeColor((int) colors[0][1]);
+                            }
+                        });
+                        errors.remove(i);
+                        i--;
+                    }
+                }
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    });
     int level = 0;
     int x;
     int y;
+    int width;
+    int height;
+    LinearLayout humanSide;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,13 +100,13 @@ public class DrawActivity extends AppCompatActivity implements View.OnTouchListe
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        int width = size.x;
-        int height = size.y;
-        LinearLayout humanSide = (LinearLayout) findViewById(R.id.human_side_second);
+        width = size.x;
+        height = size.y;
+        humanSide = (LinearLayout) findViewById(R.id.human_side_second);
         LinearLayout computerSide = (LinearLayout) findViewById(R.id.computer_side_second);
         drawMatrix(x, y, humanSide, width / 2, height, true);
         drawMatrix(x, y, computerSide, width / 2, height, false);
-
+        errorThread.start();
     }
 
     public void drawMatrix(int x, int y, LinearLayout mainLayout, int width, int height, boolean ishuman) {
@@ -80,7 +115,7 @@ public class DrawActivity extends AppCompatActivity implements View.OnTouchListe
             mainLayout.addView(tr);
             tr.setOrientation(LinearLayout.HORIZONTAL);
             for (int j = 0; j < x; j++) {
-                 a = 100;
+                a = 100;
                 if ((height / y) > (width / x)) {
                     a = (int) (width * 0.9 / x);
 
@@ -89,7 +124,7 @@ public class DrawActivity extends AppCompatActivity implements View.OnTouchListe
 
                 }
                 if (levels[level][i][j].length > 1) {
-                    TriangleView view = new TriangleView(this, (boolean)levels[level][i][j][0], (Integer) colors[(int)levels[level][i][j][1]][1], (Integer) colors[(int)levels[level][i][j][2]][1], a, a);
+                    TriangleView view = new TriangleView(this, (boolean) levels[level][i][j][0], (Integer) colors[(int) levels[level][i][j][1]][1], (Integer) colors[(int) levels[level][i][j][2]][1], a, a);
 
                     view.setPadding(a / 20, a / 20, a / 20, a / 20);
                     tr.addView(view, new ActionBar.LayoutParams(a, a));
@@ -99,8 +134,8 @@ public class DrawActivity extends AppCompatActivity implements View.OnTouchListe
                         view.setOnTouchListener(this);
 
                     }
-                }else{
-                    RectView view = new RectView(this, (Integer) colors[(int)levels[level][i][j][0]][1],a,a);
+                } else {
+                    RectView view = new RectView(this, (Integer) colors[(int) levels[level][i][j][0]][1], a, a);
                     view.setPadding(a / 20, a / 20, a / 20, a / 20);
                     tr.addView(view, new ActionBar.LayoutParams(a, a));
                     if (ishuman) {
@@ -115,7 +150,6 @@ public class DrawActivity extends AppCompatActivity implements View.OnTouchListe
     }
 
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.draw_activity_actions, menu);
@@ -125,6 +159,10 @@ public class DrawActivity extends AppCompatActivity implements View.OnTouchListe
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
+            case R.id.removeAll:
+                humanSide.removeAllViews();
+                drawMatrix(x, y, humanSide, width / 2, height, true);
+                return true;
             case R.id.action_help:
                 AlertDialog.Builder builder = new AlertDialog.Builder(DrawActivity.this);
                 builder.setTitle("Справка")
@@ -143,11 +181,11 @@ public class DrawActivity extends AppCompatActivity implements View.OnTouchListe
 
 
             default:
-for(int i=0;i<colors.length;i++){
-    if(item.getTitle().equals(colors[i][0])){
-        currentColor= (int) colors[i][1];
-    }
-}
+                for (int i = 0; i < colors.length; i++) {
+                    if (item.getTitle().equals(colors[i][0])) {
+                        currentColor = (int) colors[i][1];
+                    }
+                }
                 // If we got here, the user's action was not recognized.
                 // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
@@ -157,15 +195,24 @@ for(int i=0;i<colors.length;i++){
 
     @Override
     public boolean onTouch(View view, MotionEvent motionEvent) {
-        if(motionEvent.getAction()==MotionEvent.ACTION_DOWN){
-            if(view.getClass()==TriangleView.class){
-                if(((TriangleView)view).isLeftTop){
-                   ((TriangleView) view).changeColor(motionEvent.getX()<motionEvent.getY(),currentColor);
-                }else{
-                    ((TriangleView) view).changeColor(a-motionEvent.getX()>motionEvent.getY(),currentColor);
+        if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+            int i = humanSide.indexOfChild((LinearLayout) view.getParent());
+            int j = ((LinearLayout) view.getParent()).indexOfChild(view);
+            if (view.getClass() == TriangleView.class) {
+                if (((TriangleView) view).isLeftTop) {
+                    ((TriangleView) view).changeColor(motionEvent.getX() < motionEvent.getY(), currentColor);
+                } else {
+                    ((TriangleView) view).changeColor(a - motionEvent.getX() > motionEvent.getY(), currentColor);
                 }
-            }else{
-                ((RectView)view).changeColor(currentColor);
+            } else {
+                ((RectView) view).changeColor(currentColor);
+                if (currentColor != (int) colors[(int) levels[level][i][j][0]][1]) {
+                    ArrayList<Object> error = new ArrayList<>();
+                    error.add(i);
+                    error.add(j);
+                    error.add(0.1f);
+                    errors.add(error);
+                }
             }
         }
         return false;
