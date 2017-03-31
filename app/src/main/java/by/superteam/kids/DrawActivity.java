14c2131dbf2443e36d7177dde5bc9ff40665c2e5
Kolovaitis <kna.209@yandex.ru>
2017-03-31 17:35:02 +0300

    Color choosing works

--- app/src/main/java/by/superteam/kids/DrawActivity.java
+++ app/src/main/java/by/superteam/kids/DrawActivity.java
@@ -19,25 +19,37 @@ import android.widget.TextView;
 public class DrawActivity extends AppCompatActivity implements View.OnTouchListener {
     int currentColor=Color.DKGRAY;
     int a;
+    Object[][]colors={
+            {"white",Color.WHITE},
+            {"red",Color.RED},
+            {"green",Color.GREEN},
+            {"blue",Color.BLUE},
+            {"yellow",Color.YELLOW},
+            {"cyan",Color.CYAN},
+            {"pink",Color.MAGENTA},
+            {"brown",Color.rgb(101,67,33)},
+            {"grey",Color.DKGRAY}
+    };
     public static Object[][][][] levels = {
             {
                     {
-                            {Color.WHITE}, {Color.WHITE}, {Color.DKGRAY}, {Color.DKGRAY}, {Color.WHITE}, {Color.WHITE}
+                            {0}, {0}, {8}, {8}, {0}, {0}
                     },
                     {
-                            {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}
+                            {8}, {8}, {8}, {8}, {8}, {8}
                     },
                     {
-                            {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}
+                            {8}, {8}, {8}, {8}, {8}, {8}
                     },
                     {
-                            {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}, {Color.DKGRAY}
+                            {8}, {8}, {8}, {8}, {8}, {8}
                     },
                     {
-                            {Color.WHITE}, {Color.WHITE}, {Color.DKGRAY}, {Color.DKGRAY}, {Color.WHITE}, {Color.WHITE}
+                            {0}, {0}, {8}, {8}, {0}, {0}
                     }
             }
     };
+
     int level = 0;
     int x;
     int y;
@@ -77,22 +89,22 @@ public class DrawActivity extends AppCompatActivity implements View.OnTouchListe
 
                 }
                 if (levels[level][i][j].length > 1) {
-                    TriangleView view = new TriangleView(this, (boolean)levels[level][i][j][0],(int)levels[level][i][j][1],(int)levels[level][i][j][2], a, a);
+                    TriangleView view = new TriangleView(this, (boolean)levels[level][i][j][0], (Integer) colors[(int)levels[level][i][j][1]][1], (Integer) colors[(int)levels[level][i][j][2]][1], a, a);
 
                     view.setPadding(a / 20, a / 20, a / 20, a / 20);
                     tr.addView(view, new ActionBar.LayoutParams(a, a));
                     if (ishuman) {
-                        view.changeColor(true,Color.WHITE);
-                        view.changeColor(false,Color.WHITE);
+                        view.changeColor(true, (Integer) colors[0][1]);
+                        view.changeColor(false, (Integer) colors[0][1]);
                         view.setOnTouchListener(this);
 
                     }
                 }else{
-                    RectView view = new RectView(this,(int)levels[level][i][j][0],a,a);
+                    RectView view = new RectView(this, (Integer) colors[(int)levels[level][i][j][0]][1],a,a);
                     view.setPadding(a / 20, a / 20, a / 20, a / 20);
                     tr.addView(view, new ActionBar.LayoutParams(a, a));
                     if (ishuman) {
-                        view.changeColor(Color.WHITE);
+                        view.changeColor((Integer) colors[0][1]);
                         view.setOnTouchListener(this);
 
                     }
@@ -131,6 +143,11 @@ public class DrawActivity extends AppCompatActivity implements View.OnTouchListe
 
 
             default:
+for(int i=0;i<colors.length;i++){
+    if(item.getTitle().equals(colors[i][0])){
+        currentColor= (int) colors[i][1];
+    }
+}
                 // If we got here, the user's action was not recognized.
                 // Invoke the superclass to handle it.
                 return super.onOptionsItemSelected(item);
